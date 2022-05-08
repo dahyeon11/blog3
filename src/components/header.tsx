2f46620bfe547ff47d7f4f7f7b19d7d582f20873
박다현 (Dahyeon Park) <dahyeon1712@dgu.ac.kr>
2022-05-09 03:39:06 +0900

    feat : Header 부분 css 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as Icon } from '../img/search.svg'
 
 function Header() {
 
@@ -14,6 +15,17 @@ function Header() {
                         </Title>
                     </Logo>
                 </Left>
+                <MiddleSearchInput>
+                    <InputContainer>
+                        <SearchIcon />
+                        <SearchBox>
+                            
+                        </SearchBox>
+                    </InputContainer>
+                </MiddleSearchInput>
+                <Right>
+
+                </Right>
 
             </Wrapper>
             </Container>
@@ -43,15 +55,25 @@ const Wrapper = styled.div`
 `
 
 const Left = styled.div`
-    
+    min-width: 150px;
 `
 
 const MiddleSearchInput = styled.div`
-    
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+    position: relative;
+    width: 100%;
+    overflow-x: clip;
+    margin: 0 0 0 20px;
 `
 
 const Right = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-width: 160px;
+    max-width: 160px;
+    padding: 0 100px;
+    box-sizing: border-box;
 `
 
 const Logo = styled.div`
@@ -61,9 +83,46 @@ const Logo = styled.div`
 `
 
 const Title = styled.span`
-    font-family: 'NanumSquareRound', 'sans-serif';
-    font-weight: 700;
-    font-size: 18px;
+    font-family: 'Nanum Brush Script', cursive;
+    font-weight: 400;
+    font-size: 28px;
+    margin: auto auto auto 20px;
+    line-height: 40px;
 `
 
-export default Header
+const InputContainer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border: 1px solid #ececec;
+    border-radius: 40px;
+    height: 38px;
+    color: #000;
+    position: relative;
+    box-sizing: border-box;
+    overflow: hidden;
+    max-width: 100%;
+`
+
+const SearchIcon = styled(Icon)`
+    width: 30px;
+    height: 100%;
+    margin-left: 10px;
+    margin-right: 2px;
+    
+`
+
+const SearchBox = styled.input.attrs((props) => ({
+    placeholder: '게시물 검색'
+}))`
+    border: 0;
+    background: 0 0;
+    height: 100%;
+    padding-left: 10px;
+    transition: all .2s;
+    animation-name: fade;
+    animation-duration: .3s;
+    animation-delay: .1s;
+`
+
+export default Header;
